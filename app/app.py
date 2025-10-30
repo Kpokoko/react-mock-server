f2b975b3507b51069b2d176943483593e76cf523
Semen Kolyvanov <gdasdsadasdsa@gmail.com>
2025-10-30 14:14:31 +0500

    test api

--- app/app.py
+++ app/app.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from .routes import auth, posts, chats
+from fastapi.middleware.cors import CORSMiddleware
 from .db import engine, Base
 import asyncio
 
 app = FastAPI(title="FastAPI Session")
+
+# Указываем, с каких источников разрешены запросы
+origins = [
+    "http://localhost:5173",  # React dev server
+    "http://127.0.0.1:3000",
+    "https://example.com",    # продакшн-домен
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # разрешённые источники
+    allow_credentials=True,         # разрешаем куки / авторизацию
+    allow_methods=["*"],            # разрешаем все HTTP-методы (GET, POST и т.д.)
+    allow_headers=["*"],            # разрешаем все заголовки
+)
 
 app.include_router(auth.router)
 app.include_router(posts.router)
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -32,11 +32,28 @@ async def login(user: UserCreate, response: Response, db: AsyncSession = Depends
     response.set_cookie(key="session_token", value=token, httponly=True)
     return {"message": "Logged in"}
 
-@router.get("/me", response_model=UserRead)
-async def me(request: Request, db: AsyncSession = Depends(get_db)):
-    user_id = get_current_user(request)
-    if not user_id:
-        raise HTTPException(status_code=401, detail="Not authenticated")
-    result = await db.execute(select(User).where(User.id == user_id))
+@router.get("/profile")
+async def profile(request: Request, db: AsyncSession = Depends(get_db)):
+    # user_id = get_current_user(request)
+    # if not user_id:
+    #     raise HTTPException(status_code=401, detail="Not authenticated")
+    result = await db.execute(select(User).where(User.id == 1))
     user = result.scalars().first()
-    return user
+    res = {
+        "name": "aboba",
+        "friendCount": 100,
+        "photoCount": 20,
+        "subscriberCount": 6,
+        "posts": [
+            {
+                "id": 2,
+                "user": "aboba",
+                "postTime": "2025-10-13T09:45:00.000Z",
+                "text": "scam",
+                "image": "/images/2.png",
+                "likes": 8,
+                "comments": ['Отстой']
+            }
+        ]
+    }
+    return res
